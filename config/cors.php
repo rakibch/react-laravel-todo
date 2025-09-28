30f4254aa620d646055759283c5eea5743d1e26f
rakibch <rakib9204@gmail.com>
2025-09-28 21:19:08 +0600

    line fixed

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:5173',
+    'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://localhost:8000', // add backend origin if needed
     'http://127.0.0.1:8000', // add backend origin if frontend calls from same host
